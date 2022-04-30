ea8dfb3bf235bdd2ed6cf15f4ab354f46c31383e
Vin <phamvanvinh8a2@gmail.com>
2022-04-30 15:28:08 +0700

    CTQ

--- CC++/CTDL/CTQ/EngDic.c
+++ CC++/CTDL/CTQ/EngDic.c
@@ -6,9 +6,11 @@ struct EngDic
 {
 	struct EngDic *sub[27];
 };
+
 void insertWord(struct EngDic *d, char *w)
 {
-	struct EngDic *p = d; int i,j, k;
+	struct EngDic *p = d;
+	int i,j, k;
 	for(i=0; i< strlen(w); i++)
 	{
 		j = w[i]-'a';
@@ -17,8 +19,11 @@ void insertWord(struct EngDic *d, char *w)
 		else
 		{
 			p->sub[j] = (struct EngDic*) malloc(sizeof(struct EngDic));
-			if(p->sub[j]==NULL) {
-			printf("Loiiiii!"); return;}
+			if(p->sub[j]==NULL)
+			{
+				printf("Loiiiii!");
+				return;
+			}
 			p = p->sub[j];
 			for(k = 0; k < 27; k++) p->sub[k]=NULL;
 		}
@@ -31,9 +36,11 @@ void insertWord(struct EngDic *d, char *w)
 	}
 	else printf("\nTrung %s", w);
 }
+
 int find(struct EngDic *d, char *w)
 {
-	struct EngDic *p = d; int i,j, k;
+	struct EngDic *p = d;
+	int i,j, k;
 	for(i=0; i< strlen(w); i++)
 	{
 		j = w[i]-'a';
@@ -70,29 +77,118 @@ struct EngDic* initDic()
 
 int readDic(char *fileName, struct EngDic *goc)
 {
-	FILE *f; char w[40];
- 	f = fopen(fileName,"r");
- 	if(f==NULL) printf("Loi!!!");
- 	int d = 0;
- 	while (fgets(w, 40, f) != NULL)
- 	{
- 		int k = strlen(w);
- 		if(w[k-1]=='\n') w[k-1]='\0';
- 		insertWord(goc,w);
- 		d++;
+	FILE *f;
+	char w[40];
+	f = fopen(fileName,"r");
+	if(f==NULL) printf("Loi!!!");
+	int d = 0;
+	while (fgets(w, 40, f) != NULL)
+	{
+		int k = strlen(w);
+		if(w[k-1]=='\n') w[k-1]='\0';
+		insertWord(goc,w);
+		d++;
 	}
 	fclose(f);
 	return d;
 }
+
+int max(int a, int b)
+{
+	return a >= b ? a : b;
+}
+
+int getEngDicHeight(struct EngDic *root)
+{
+ 	int i, subHeight, maxHeight = 0;
+
+	if (root != NULL)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			subHeight = 1 + getEngDicHeight(root->sub[i]);
+			maxHeight = max(maxHeight, subHeight);
+		}
+	}
+
+	return maxHeight;
+}
+
+int countWords(struct EngDic *root)
+{
+ 	int i, count = 0;
+
+	if (root != NULL)
+	{
+		if (root->sub[26] != NULL)
+		{
+			count = 1;
+		}
+
+		for (i = 0; i < 26; i++)
+		{
+			count += countWords(root->sub[i]);
+		}
+	}
+
+	return count;
+}
+
+struct EngDic *findEngDicBeginWith(struct EngDic *root, char *str)
+{
+	struct EngDic *p;
+	int i, strLength, rangeFromAToChar;
+
+	if (root != NULL)
+	{
+		p = root;
+		strLength = strlen(str);
+		for (i = 0; i < strLength; i++)
+		{
+			rangeFromAToChar = str[i] - 'a';
+			if (p->sub[rangeFromAToChar] != NULL)
+			{
+				p = p->sub[rangeFromAToChar];
+			}
+			else
+			{
+				return NULL;
+			}
+		}
+
+		return p;
+	}
+}
+
+void listWordsBeginWith(struct EngDic *root, char *str)
+{
+	struct EngDic *p = findEngDicBeginWith(root, str);
+
+	if (p != NULL)
+	{
+		printEngDic(p, str, strlen(str));
+	}
+}
+
 void main()
 {
-	struct EngDic *goc;int k; char w[40];
+	struct EngDic *goc;
+	int k;
+	char w[40];
+
 	goc = initDic();
 	printf("Loading...");
 	k = readDic("words_alpha.txt", goc);
 	printf("%d words finished.\n",k);
-	printf("Searching...\n");
-	k = find(goc, "dichlorodiphenyltrichloroethane");
-	printf("\n%d", k);
+	printf("\nSearching...");
+	k = find(goc, "inher");
+	printf("\nSearch result: %d\n", k);
+	k = getEngDicHeight(goc);
+	printf("\nHeight: %d\n", k);
+	k = countWords(goc);
+	printf("\nWords: %d\n\n", k);
+ 	strcpy(w, "inher");
+	listWordsBeginWith(goc, w);
+	strcpy(w, "");
 	printEngDic(goc, w, 0);
 }
